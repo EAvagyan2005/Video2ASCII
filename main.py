6a05e2cfd94b1c351515b5f2d073bf3b877367f2
AlwaysLucky2005 <whatisthisyooutube@gmail.com>
2022-11-07 18:19:59 +0400

    First version of converter Bad apple video Some Fixes

--- main.py
+++ main.py
@@ -1,38 +1,36 @@
 import os
 import cv2
 
-# video = 24:18, cmd 6:1 => 48*3:18
 
 cam_or_vid = input("Enter the name of the video(bad_apple.mp4)")
 cam_or_vid = int(cam_or_vid) if cam_or_vid.isdigit() else cam_or_vid
+### GETTING STREAM OF VIDEO
 vid = cv2.VideoCapture(cam_or_vid)
+
 dsize1 = tuple(([int(x) for x in input("Enter the width and height(144 22): ").split()]))
+
+### ASSETS FROM GRAYSCALE
 grayscale_str1 = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
 grayscale_str = " .,:ilwW"
 # os.system(f"mode con cols={dsize1[0]} lines={dsize1[1]}")
 # file = open('new_file.txt', 'w')
+
+
 if not vid.isOpened():
     print("Error opening video stream or file!")
 while vid.isOpened():
     ret, frame = vid.read()
+    ### CONVERTING TO THE GRAYSCALE
     frame_1 = frame
     frame = cv2.resize(frame, dsize1)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rows, cols = dsize1[::-1]
     os.system("cls")
-    """file.close()
-    file = open("new_file.txt", "w")
-    os.system("tasklist > file.txt")
-    ls = open("file.txt", 'r')
-    if "notepad.exe" in ls:
-        os.system("taskkill/im notepad.exe")"""
     for k in range(rows):
         for l in range(cols):
             print(grayscale_str[int((frame[k, l] * (len(grayscale_str)-1)/255))], end='')
-            # file.write(grayscale_str[int((frame[k, l] * (len(grayscale_str)-1)/255))])
         print('\n')
-        #file.write('\n')
-    ##os.system("notepad.exe new_file.txt")
+
     if ret:
         cv2.imshow("Frame", frame_1)
         if cv2.waitKey(2) & 0xFF == ord('q'):
